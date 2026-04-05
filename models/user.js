81401547b2d7311ddc4a53dc516879f20d29da7a
Fiama de Capua <42975721+FihCapua@users.noreply.github.com>
2026-04-05 20:02:54 +0000

    feat: create user model and /api/v1/users endpoint

--- models/user.js
+++ models/user.js
@@ -1,8 +1,58 @@
 import database from "infra/database";
+import { ValidationError } from "infra/errors.js";
 
 async function create(userInputValues) {
+  await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUserName(userInputValues.username);
+
+  const newUser = await runInsertQuery(userInputValues);
+  return newUser;
+
+  async function validateUniqueEmail(email) {
     const result = await database.query({
-        text: `
+      text: `
+            SELECT
+                email 
+            FROM
+                users
+            WHERE
+                LOWER(email) = LOWER($1)
+            ;`,
+      values: [email],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError({
+        message: "O email informado já está sendo utilizado.",
+        action: "Utilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUserName(username) {
+    const result = await database.query({
+      text: `
+            SELECT
+                username 
+            FROM
+                users
+            WHERE
+                LOWER(username) = LOWER($1)
+            ;`,
+      values: [username],
+    });
+
+    if (result.rowCount > 0) {
+      throw new ValidationError({
+        message: "O usuário informado já está sendo utilizado.",
+        action: "Utilize outro nome de usuário para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function runInsertQuery(userInputValues) {
+    const result = await database.query({
+      text: `
             INSERT INTO 
                 users (username, email, password) 
             VALUES 
@@ -10,14 +60,19 @@ async function create(userInputValues) {
             RETURNING
                 *
             ;`,
-        values: [userInputValues.username, userInputValues.email, userInputValues.password]
-    })
+      values: [
+        userInputValues.username,
+        userInputValues.email,
+        userInputValues.password,
+      ],
+    });
 
-    return result.rows[0]
+    return result.rows[0];
+  }
 }
 
 const user = {
-    create
-}
+  create,
+};
 
 export default user;
--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,6 @@
 import { createRouter } from "next-connect";
-import controller from "infra/controller.js"
-import user from "models/user.js"
+import controller from "infra/controller.js";
+import user from "models/user.js";
 
 const router = createRouter();
 
@@ -10,8 +10,8 @@ export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
   const userInputValues = request.body;
-  console.log(userInputValues)
+  console.log(userInputValues);
   const newUser = await user.create(userInputValues);
-  console.log(newUser)
+  console.log(newUser);
   return response.status(201).json(newUser);
 }
